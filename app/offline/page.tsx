ea73a66dd6878359b5477cedaf51f79d93bbcbfc
quinterodiego <d86webs@gmail.com>
2026-07-06 16:25:55 -0300

    fix miscellanious

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function OfflinePage() {
   return (
     <div className="min-h-screen bg-[#f7f9fc] dark:bg-gray-900 flex flex-col items-center justify-center px-6 text-center">
